6ced090634c3cd886f5cc7c13c4183ee01db92cc
mfrugoni <miguelmfrugoni@gmail.com>
2023-08-03 16:36:03 -0300

    V3: Implement client class with atributes and methods

--- src/main/java/com/ap/homebanking/models/Client.java
+++ src/main/java/com/ap/homebanking/models/Client.java
@@ -1,4 +1,49 @@
 package com.ap.homebanking.models;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Client {
+    @Id
+    private long Id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public Client(){}
+
+    public Client(String firstName, String lastName, String email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public long getId() {
+        return Id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
