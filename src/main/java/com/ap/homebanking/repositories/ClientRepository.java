ff8c241fdc0c290878d5037dae1f1ffa3d1dc503
mfrugoni <miguelmfrugoni@gmail.com>
2023-08-03 16:53:46 -0300

    V4: implement clientrepo as a rest repository

--- src/main/java/com/ap/homebanking/repositories/ClientRepository.java
+++ src/main/java/com/ap/homebanking/repositories/ClientRepository.java
@@ -1,4 +1,9 @@
 package com.ap.homebanking.repositories;
 
-public interface ClientRepository {
+import com.ap.homebanking.models.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ClientRepository extends JpaRepository<Client, Long> {
 }
