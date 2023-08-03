ff65e106e335cb5a0cdc9be76ab19c86915d2354
mfrugoni <miguelmfrugoni@gmail.com>
2023-08-03 18:35:57 -0300

    V7: Add CommandLineRunner to springbootapplication

--- src/main/java/com/ap/homebanking/HomebankingApplication.java
+++ src/main/java/com/ap/homebanking/HomebankingApplication.java
@@ -1,5 +1,8 @@
 package com.ap.homebanking;
 
+import com.ap.homebanking.models.Client;
+import com.ap.homebanking.repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +11,21 @@ public class HomebankingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomebankingApplication.class, args);
+	}
+
+	public CommandLineRunner initData(ClientRepository clientRepository){
+		return (args -> {
+
+			Client client1 = new Client();
+			client1.setFirstName("Melba");
+			client1.setLastName("Morel");
+			client1.setEmail("melba@mindhub.com");
+
+			Client client2 = new Client("Miguel", "Frugoni", "mail@mail.com");
+
+			clientRepository.save(client1);
+			clientRepository.save(client2);
+		});
 	}
 
 }
