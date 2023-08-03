77c109f3d37608d4fd30a56e7d45c6cee4c0c172
mfrugoni <miguelmfrugoni@gmail.com>
2023-08-03 17:52:10 -0300

    V6: Add generated value to id

--- src/main/java/com/ap/homebanking/models/Client.java
+++ src/main/java/com/ap/homebanking/models/Client.java
@@ -1,11 +1,17 @@
 package com.ap.homebanking.models;
 
+import org.hibernate.annotations.GenericGenerator;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Client {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
     private long Id;
     private String firstName;
     private String lastName;
