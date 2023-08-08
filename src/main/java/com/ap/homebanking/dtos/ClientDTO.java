3854038b07b12ff371f406fc5a7d736ed5c797e2
mfrugoni <miguelmfrugoni@gmail.com>
2023-08-07 22:08:22 -0300

    V21: implement clientdto att and methods

--- src/main/java/com/ap/homebanking/dtos/ClientDTO.java
+++ src/main/java/com/ap/homebanking/dtos/ClientDTO.java
@@ -1,4 +1,42 @@
 package com.ap.homebanking.dtos;
 
+import com.ap.homebanking.models.Account;
+import com.ap.homebanking.models.Client;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class ClientDTO {
+    private long Id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    //Set<Account> accounts = new HashSet<>();
+
+    public ClientDTO(Client client){
+        this.Id = client.getId();
+
+        this.firstName = client.getFirstName();
+
+        this.lastName = client.getLastName();
+
+        this.email = client.getEmail();
+    }
+
+    public long getId() {
+        return Id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
