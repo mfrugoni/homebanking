a86de91808e9f626a4cdf0c2e05364c640198d81
mfrugoni <miguelmfrugoni@gmail.com>
2023-08-08 21:31:47 -0300

    V29: Implement Transaction

--- src/main/java/com/ap/homebanking/models/Transaction.java
+++ src/main/java/com/ap/homebanking/models/Transaction.java
@@ -1,4 +1,66 @@
 package com.ap.homebanking.models;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+    private TransactionType type;
+    private double amount;
+    private String description;
+    private LocalDateTime date;
+
+    public Transaction(){}
+
+    public Transaction(TransactionType type, double amount, String description, LocalDateTime date) {
+        this.type = type;
+        this.amount = amount;
+        this.description = description;
+        this.date = date;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
 }
