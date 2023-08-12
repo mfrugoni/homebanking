30103e309127a21f9b433829ec260065edfa43bc
mfrugoni <miguelmfrugoni@gmail.com>
2023-08-11 21:37:20 -0300

    V44: Create ClientLoan class

---
+++ src/main/java/com/ap/homebanking/models/ClientLoan.java
@@ -0,0 +1,21 @@
+package com.ap.homebanking.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class ClientLoan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+    private double amount;
+    private int payments;
+    private Client client;
+    private Loan loan;
+
+}
