4f9271dab9e6dd0c39a7e8c0f5ed2ebb5fe977c2
mfrugoni <miguelmfrugoni@gmail.com>
2023-08-15 19:18:45 -0300

    V56: Implement Card class

--- src/main/java/com/ap/homebanking/models/Card.java
+++ src/main/java/com/ap/homebanking/models/Card.java
@@ -1,4 +1,102 @@
 package com.ap.homebanking.models;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
 public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+    private String cardHolder;
+    private CardType type;
+    private CardColor color;
+    private long number;
+    private short cvv;
+    private LocalDate fromDate;
+    private LocalDate thruDate;
+
+    public Card(){}
+
+    public Card(String cardHolder,
+                CardType type,
+                CardColor color,
+                long number,
+                short cvv,
+                LocalDate fromDate,
+                LocalDate thruDate) {
+        this.cardHolder = cardHolder;
+        this.type = type;
+        this.color = color;
+        this.number = number;
+        this.cvv = cvv;
+        this.fromDate = fromDate;
+        this.thruDate = thruDate;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getCardHolder() {
+        return cardHolder;
+    }
+
+    public void setCardHolder(String cardHolder) {
+        this.cardHolder = cardHolder;
+    }
+
+    public CardType getType() {
+        return type;
+    }
+
+    public void setType(CardType type) {
+        this.type = type;
+    }
+
+    public CardColor getColor() {
+        return color;
+    }
+
+    public void setColor(CardColor color) {
+        this.color = color;
+    }
+
+    public long getNumber() {
+        return number;
+    }
+
+    public void setNumber(long number) {
+        this.number = number;
+    }
+
+    public short getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(short cvv) {
+        this.cvv = cvv;
+    }
+
+    public LocalDate getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(LocalDate fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public LocalDate getThruDate() {
+        return thruDate;
+    }
+
+    public void setThruDate(LocalDate thruDate) {
+        this.thruDate = thruDate;
+    }
 }
