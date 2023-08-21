f68b400d0006c24ac231ee9bda1515af7b6608c5
mfrugoni <miguelmfrugoni@gmail.com>
2023-08-20 21:57:13 -0300

    V70: Create password encoder at web auth class

--- src/main/java/com/ap/homebanking/configurations/WebAuthentication.java
+++ src/main/java/com/ap/homebanking/configurations/WebAuthentication.java
@@ -3,12 +3,15 @@ package com.ap.homebanking.configurations;
 import com.ap.homebanking.models.Client;
 import com.ap.homebanking.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebAuthentication extends GlobalAuthenticationConfigurerAdapter {
@@ -36,6 +39,14 @@ public class WebAuthentication extends GlobalAuthenticationConfigurerAdapter {
             }
 
         });
+
+    }
+
+    @Bean
+
+    public PasswordEncoder passwordEncoder() {
+
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
     }
 
