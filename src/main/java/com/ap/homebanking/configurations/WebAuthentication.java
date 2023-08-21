020127faaf2e12e5e0c55eebb773747de416c9c0
mfrugoni <miguelmfrugoni@gmail.com>
2023-08-20 21:55:06 -0300

    V69: Implement web auth class

--- src/main/java/com/ap/homebanking/configurations/WebAuthentication.java
+++ src/main/java/com/ap/homebanking/configurations/WebAuthentication.java
@@ -1,14 +1,42 @@
 package com.ap.homebanking.configurations;
 
+import com.ap.homebanking.models.Client;
 import com.ap.homebanking.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @Configuration
 public class WebAuthentication extends GlobalAuthenticationConfigurerAdapter {
     @Autowired
     ClientRepository clientRepository;
 
+    @Override
+
+    public void init(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.userDetailsService(inputName-> {
+
+            Client client = clientRepository.findByEmail(inputName);
+
+            if (client != null) {
+
+                return new User(client.getEmail(), client.getPassword(),
+
+                        AuthorityUtils.createAuthorityList("CLIENT"));
+
+            } else {
+
+                throw new UsernameNotFoundException("Unknown user: " + inputName);
+
+            }
+
+        });
+
+    }
 
 }
