570338d79d958f376f01cdbcfc17f760feab1385
mfrugoni <miguelmfrugoni@gmail.com>
2023-08-24 22:55:36 -0300

    V82: Implement createAccount servlet at AccountController with getRandomNumber - authentication - 3 accounts limit

--- src/main/java/com/ap/homebanking/controllers/AccountController.java
+++ src/main/java/com/ap/homebanking/controllers/AccountController.java
@@ -1,13 +1,23 @@
 package com.ap.homebanking.controllers;
 
 import com.ap.homebanking.dtos.AccountDTO;
+import com.ap.homebanking.models.Account;
+import com.ap.homebanking.models.Client;
 import com.ap.homebanking.repositories.AccountRepository;
+import com.ap.homebanking.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static java.util.stream.Collectors.toList;
 
@@ -17,6 +27,12 @@ public class AccountController {
     @Autowired
     private AccountRepository accountRepository;
 
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Autowired
+    private Authentication authentication;
+
     @RequestMapping("/accounts")
     public List<AccountDTO> getAccounts(){
         return accountRepository.findAll().stream().map(account -> new AccountDTO(account)).collect(toList());
@@ -25,6 +41,31 @@ public class AccountController {
     @RequestMapping("/accounts/{id}")
     public AccountDTO getAccount(@PathVariable Long id){
         return accountRepository.findById(id).map(account -> new AccountDTO(account)).orElse(null);
+    }
+
+    @RequestMapping(path = "/clients/current/accounts", method = RequestMethod.POST)
+    public ResponseEntity<Object> createAccount(){
+        Client authenticated = clientRepository.findByEmail(authentication.getName());
+
+        Set<Account> totalAccounts = new HashSet<>();
+        totalAccounts = authenticated.getAccounts();
+
+        if (totalAccounts.size() >= 3)
+            return new ResponseEntity<>("You can´t own more than three accounts", HttpStatus.FORBIDDEN);
+
+        else {
+            Double number = getRandomNumber(0, 99999999);
+            String accountNumber = "VIN-" + number.toString();
+
+            Account account = new Account(accountNumber, LocalDate.now(), 0 );
+            authenticated.addAccount(account);
+            accountRepository.save(account);
+            return new ResponseEntity<>("New account created", HttpStatus.CREATED);
+        }
+    }
+
+    public double getRandomNumber(double min, double max){
+        return ((Math.random() * (max - min)) + min);
     }
 
 }
