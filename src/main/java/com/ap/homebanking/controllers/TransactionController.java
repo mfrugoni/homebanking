492f84b4bd0f1705743fb5df6145e1b59922d6a9
mfrugoni <miguelmfrugoni@gmail.com>
2023-08-29 21:11:09 -0300

    V95: Create transaction controller

---
+++ src/main/java/com/ap/homebanking/controllers/TransactionController.java
@@ -0,0 +1,9 @@
+package com.ap.homebanking.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TransactionController {
+}
