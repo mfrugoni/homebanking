c7d246594ffbb6b0343c5ec3f4822ac2a36d7d5d
mfrugoni <miguelmfrugoni@gmail.com>
2023-08-29 21:30:31 -0300

    V96: Add new front end files

--- src/main/java/com/ap/homebanking/controllers/TransactionController.java
+++ src/main/java/com/ap/homebanking/controllers/TransactionController.java
@@ -1,9 +1,16 @@
 package com.ap.homebanking.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class TransactionController {
+    @RequestMapping(value = "/transactions", method = RequestMethod.POST)
+    public ResponseEntity<Object> createTransaction(@RequestParam double amount, String description, String number, String number){
+
+    }
 }
