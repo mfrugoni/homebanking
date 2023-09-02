61495246709ca20c53b9a49dc97b3ea8e4d550d3
mfrugoni <miguelmfrugoni@gmail.com>
2023-09-02 17:33:16 -0300

    V102: Add getRandomNumbers to Util

--- src/main/java/com/ap/homebanking/utils/Util.java
+++ src/main/java/com/ap/homebanking/utils/Util.java
@@ -1,4 +1,9 @@
 package com.ap.homebanking.utils;
 
 public class Util {
+
+    public static int getRandomNumber(int min, int max){
+        return (int) ((Math.random() * (max - min)) + min);
+    }
+
 }
