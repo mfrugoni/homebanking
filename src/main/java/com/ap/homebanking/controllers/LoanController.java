429e917cffa5206e0f5d98968982e3492f8e5f37
mfrugoni <miguelmfrugoni@gmail.com>
2023-09-02 18:14:19 -0300

    V105: Create LoanController

---
+++ src/main/java/com/ap/homebanking/controllers/LoanController.java
@@ -0,0 +1,11 @@
+package com.ap.homebanking.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class LoanController {
+    @RequestMapping("/loans")
+
+}
