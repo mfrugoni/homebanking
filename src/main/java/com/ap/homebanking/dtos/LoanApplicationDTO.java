0be48349b3446cd37d2001f43cb906846ec452dc
mfrugoni <miguelmfrugoni@gmail.com>
2023-09-02 18:34:00 -0300

    V108: Implement LoanApplicationDTO - create LoanDTO

--- src/main/java/com/ap/homebanking/dtos/LoanApplicationDTO.java
+++ src/main/java/com/ap/homebanking/dtos/LoanApplicationDTO.java
@@ -1,4 +1,24 @@
 package com.ap.homebanking.dtos;
 
 public class LoanApplicationDTO {
+private int loanId;
+private double amount;
+private int payments;
+private String toAccountNumber;
+
+    public int getLoanId() {
+        return loanId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public int getPayments() {
+        return payments;
+    }
+
+    public String getToAccountNumber() {
+        return toAccountNumber;
+    }
 }
