68483532ebf1cca0dc1ac6fd9f741e3e3c2804fa
mfrugoni <miguelmfrugoni@gmail.com>
2023-09-02 18:54:38 -0300

    V110: Implement and try getLoans at LoanController

--- src/main/java/com/ap/homebanking/controllers/LoanController.java
+++ src/main/java/com/ap/homebanking/controllers/LoanController.java
@@ -1,11 +1,24 @@
 package com.ap.homebanking.controllers;
 
+import com.ap.homebanking.dtos.LoanDTO;
+import com.ap.homebanking.repositories.LoanRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
 public class LoanController {
+
+    @Autowired
+    private LoanRepository loanRepository;
+
     @RequestMapping("/loans")
+    public List<LoanDTO> getLoans(){
+        return loanRepository.findAll().stream().map(loan -> new LoanDTO(loan)).collect(Collectors.toList());
+    }
 
 }
