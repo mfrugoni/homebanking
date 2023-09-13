86af6ec3a6098e980fc666bc24c221f875e7c32b
mfrugoni <miguelmfrugoni@gmail.com>
2023-09-12 23:06:56 -0300

    V127: Create repositories tests class for integration tests

---
+++ src/test/java/com/ap/homebanking/RepositoriesTest.java
@@ -0,0 +1,35 @@
+package com.ap.homebanking;
+
+import com.ap.homebanking.models.Loan;
+import com.ap.homebanking.repositories.LoanRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+
+//@DataJpaTest
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@SpringBootTest
+public class RepositoriesTest {
+    @Autowired
+    LoanRepository loanRepository;
+
+    @Test
+    public void existLoans(){
+        List<Loan> loans = loanRepository.findAll();
+        assertThat(loans,is(not(empty())));
+    }
+
+    @Test
+    public void existPersonalLoan(){
+        List<Loan> loans = loanRepository.findAll();
+        assertThat(loans, hasItem(hasProperty("name", is("Personal loan"))));
+    }
+}
